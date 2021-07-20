89914609ed6518d7ff3c937af7b81393bc931c48
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-07-20 20:45:09 +0900

    movie_app_props

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Food = ({ name, picture }) => {
+const Food = ({ name, picture, rating }) => {
   return (
     <div>
-      <h2>I love {name}</h2>;
-      <img src={picture} />;
+      <h2>I love {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
     </div>
   );
 };
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image: "https://pbs.twimg.com/media/CUvEAsTUcAANUnd.jpg",
+    rating: 5,
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "http://static.news.zumst.com/images/2/2017/02/26/2030c76e427f4c278c16ab0eb81bd45d.jpg",
+    rating: 4.9,
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "https://ext.fmkorea.com/files/attach/new/20160118/3655304/26867920/295252026/079374140f849a679002fdfc804ad300.jpg",
+    rating: 4.5,
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "http://mbcmall.imbc.com/store/__icsFiles/afieldfile/2018/12/27/001_1.jpg",
+    rating: 4.8,
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://recipe.ezmember.co.kr/cache/recipe/2015/04/04/74948292d9f3b94cf248bab1c2ce71f21.jpg",
+    rating: 4.4,
   },
 ];
 
@@ -40,10 +52,21 @@ function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 export default App;
